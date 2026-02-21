8c6079a948137282ab7103a83c6ac3fc57e18aaf
Justin <justinbondc322@gmail.com>
2026-02-21 09:25:02 +0800

    Added dotenv to add security for handling database connection uri.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+const PORT = process.env.PORT || 3000
 
 // Temporarily allows all requests, will restrict later on for security.
 app.use(cors())
 
-const app = express()
-const PORT = 3000
+mongoose.connect(process.env.MONGODB_CONNECTION)
+.then(()=> {console.log("Database connection success!");
+})
+.catch((error)=> {console.log("Database connection failed! Error: " + error);
+})
 
 app.listen(PORT, (error) => {
     if(!error){
