84b548713608f459d04d0b7235537fde2daa4c97
Justin <justinbondc322@gmail.com>
2026-02-21 09:41:21 +0800

    Created Syllabus model.

---
+++ src/models/Syllabus.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const syllabusSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    code: {
+        type: String,
+        require: true
+    },
+    instructor: {
+        type: String,
+        require: true
+    },
+    semester: {
+        type: String,
+    },
+    events: eventSchema,
+    grading: gradeSchema,
+}, {
+    timestamps: true
+});
+
+const Syllabus = mongoose.model('Syllabus', syllabusSchema);
+export default Syllabus;
