9bae757ae692471becc9335ca477cf8181f1f5ca
shamika <shamanne.sawalha@gmail.com>
2026-03-10 07:57:31 +0800

    calendar from svelte component

---
+++ user_interface/sylla-ui/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import Calendar from './PCalendar.svelte';
+</script>
+
+<main>
+  <div>
+    <Calendar />
+  </div>
+</main>
+
+<style>
+</style>
---
+++ user_interface/sylla-ui/src/PCalendar.svelte
@@ -0,0 +1,12 @@
+<script>
+    import {Calendar, DayGrid} from '@event-calendar/core';
+
+    let options = $state({
+        view: 'dayGridMonth',
+        events: [
+            // your list of events
+        ]
+    });
+</script>
+
+<Calendar plugins={[DayGrid]} {options} />
