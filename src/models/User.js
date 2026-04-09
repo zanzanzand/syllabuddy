c262bda8afd18715d11dde3ec06d3115299d400a
dptkdl <julieannxyrene.lalimarmo@gmail.com>
2026-04-09 22:21:01 +0800

    Added fields for theme and category colors

--- src/models/User.js
+++ src/models/User.js
@@ -18,6 +18,32 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ''
     },
+    calendarTheme: {
+        type: String,
+        enum: ['light', 'dark', 'high-contrast'],
+        default: 'light'
+    },
+    calendarBackground: {
+        type: String,
+        default: ''
+    },
+    backgroundOpacity: {
+        type: Number,
+        default: 1,
+        min: 0,
+        max: 1
+    },
+    categoryColors: {
+        type: Map,
+        of: String,
+        default: {
+            'exam': '#FF6B6B',
+            'assignment': '#4ECDC4',
+            'project': '#45B7D1',
+            'quiz': '#96CEB4',
+            'other': '#DDA0DD' 
+        }
+    }
 }, {
     timestamps: true
 });
